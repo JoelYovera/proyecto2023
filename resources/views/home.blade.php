d0c957375f58ce497916f537395436d153178fb3
joelyovera <joelyovera9@gmail.com>
2023-09-19 12:11:46 -0500

    create

---
+++ resources/views/home.blade.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Sitio oficial de KIKE</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+</head>
+
+<body>
+    <header>
+        <div class="col-8">
+            <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top nav-responsive px-2 py-2">
+                <div class="container">
+                    <a class="navbar-brand" href="#">HOME</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="navbar-collapse collapse" id="navbarNav">
+                        <form class="form-inline my-2 my-lg-0" id="formulario-busqueda">
+                            <div class="input-group ml-auto">
+                                <input class="form-control rounded-pill" type="search" placeholder="Buscar"
+                                    aria-label="Search">
+                                <div class="input-group-append">
+                                    <span class="input-group-text">
+                                        <i class="fas fa-search"></i>
+                                    </span>
+                                </div>
+                            </div>
+                        </form>
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Buscar una tienda</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">Ayuda <i class="fa fa-help"></i></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Unirse a Nosotros</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Iniciar sesión</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+
+    <div class="container">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+            <div class="container">
+                <div class="navbar-collapse collapse">
+                    <ul class="navbar-nav mx-auto align-items-center">
+                        <li class="nav-item Menu1">
+                            <a class="nav-link" data-toggle="collapse" href="#collapseExample" role="button"
+                                aria-expanded="false" aria-controls="collapseExample">Novedades y Destacados</a>
+                            <div class="collapse" id="collapseExample">
+                                <div class="card card-body">
+                                    <div class="container-fluid cew-9">
+                                        <div class="row">
+                                            <div class="col">
+                                                <a class="navbar-brand">Destacados</a>
+                                                <a class="nav-link" href="#">Comprar Novedades</a>
+                                                <a class="nav-link" href="#">Novedades en ropa</a>
+                                                <a class="nav-link" href="#">Novedades en zapatillas</a>
+                                                <a class="nav-link" href="#">Member Shop</a>
+                                                <a class="nav-link" href="#">Productos de Invierno</a>
+                                            </div>
+                                            <div class="col">
+                                                <a class="navbar-brand">Destacados</a>
+                                                <a class="nav-link" href="#">Zapatillas</a>
+                                                <a class="nav-link" href="#">Ropa elegante</a>
+                                                <a class="nav-link" href="#">Deporte</a>
+                                                <a class="nav-link" href="#">Relojes</a>
+                                                <a class="nav-link" href="#">Ropa Casual</a>
+                                            </div>
+                                            <div class="col">
+                                                <a class="navbar-brand">Destacados</a>
+                                                <a class="nav-link" href="#">Zapatillas</a>
+                                                <a class="nav-link" href="#">Ropa elegante</a>
+                                                <a class="nav-link" href="#">Deporte</a>
+                                                <a class="nav-link" href="#">Relojes</a>
+                                                <a class="nav-link" href="#">Ropa Casual</a>
+                                            </div>
+                                            <div class="col">
+                                                <a class="navbar-brand">Destacados</a>
+                                                <a class="nav-link" href="#">Zapatillas</a>
+                                                <a class="nav-link" href="#">Ropa elegante</a>
+                                                <a class="nav-link" href="#">Deporte</a>
+                                                <a class="nav-link" href="#">Relojes</a>
+                                                <a class="nav-link" href="#">Ropa Casual</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="nav-item Menu2">
+                            <a class="nav-link" data-toggle="collapse" href="#collapseExample1" role="button"
+                                aria-expanded="false" aria-controls="collapseExample1">Hombre</a>
+                            <div class="collapse" id="collapseExample1">
+                                <div class="card card-body">
+                                    <div class="container-fluid cew-9">
+                                        <div class="row">
+                                            <div class="col">
+                                                <a class="navbar-brand" href="#">Para hombre</a>
+                                                <a class="nav-link" href="#">Zapatillas</a>
+                                                <a class="nav-link" href="#">Ropa elegante</a>
+                                                <a class="nav-link" href="#">Deporte</a>
+                                                <a class="nav-link" href="#">Relojes</a>
+                                                <a class="nav-link" href="#">Ropa Casual</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="nav-item Menu3">
+                            <a class="nav-link" data-toggle="collapse" href="#collapseExample2" role="button"
+                                aria-expanded="false" aria-controls="collapseExample2">Mujer</a>
+                            <div class="collapse" id="collapseExample2">
+                                <div class="card card-body">
+                                    <div class="container-fluid cew-9">
+                                        <a class="navbar-brand" href="#">Para Ellas</a>
+                                        <a class="nav-link" href="#">Zapatos/Tacos</a>
+                                        <a class="nav-link" href="#">Ropa y Vestidos</a>
+                                        <a class="nav-link" href="#">Maquillaje</a>
+                                        <a class="nav-link" href="#">Bolsas</a>
+                                        <a class="nav-link" href="#">Deporte</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="nav-item Menu4">
+                            <a class="nav-link" data-toggle="collapse" href="#collapseExample3" role="button"
+                                aria-expanded="false" aria-controls="collapseExample3">Niño/a</a>
+                            <div class="collapse" id="collapseExample3">
+                                <div class="card card-body">
+                                    <div class="container-fluid cew-9">
+                                        <a class="navbar-brand" href="#">Para los pequeños</a>
+                                        <a class="nav-link" href="#">Ropa</a>
+                                        <a class="nav-link" href="#">Zapatillas</a>
+                                        <a class="nav-link" href="#">Juguetes</a>
+                                        <a class="nav-link" href="#"></a>
+                                        <a class="nav-link" href="#"></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="nav-item Menu5">
+                            <a class="nav-link" data-toggle="collapse" href="#collapseExample4" role="button"
+                                aria-expanded="false" aria-controls="collapseExample4">Ofertas</a>
+                            <div class="collapse" id="collapseExample4">
+                                <div class="card card-body">
+                                    <div class="container-fluid cew-9">
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</body>
+
+</html>
+
+<style>
+    #formulario-busqueda {
+        margin-left: 1100px;
+    }
+
+    body {
+        padding-top: 70px;
+        padding-bottom: 70px;
+    }
+
+    .form-control.rounded-pill {
+        border-radius: 50px;
+    }
+
+    .input-group-text {
+        background: transparent;
+        border: none;
+        padding: 100;
+        position: absolute;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        line-height: inherit;
+        display: flex;
+        align-items: center;
+        cursor: pointer;
+    }
+</style>
+
+
+<script>
+	
+</script>
